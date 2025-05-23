82f32296653f6b8683604d2f9102048bd4f854d1
tngh4037 <tngh4037@gmail.com>
2025-05-23 19:03:11 +0900

    init

---
+++ src/main/java/com/example/coupon_backend/CouponBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponBackendApplication.class, args);
+	}
+
+}
